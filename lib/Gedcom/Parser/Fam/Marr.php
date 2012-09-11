2a6ef491cab6e096fd7e457bb6de65a8ace80391
Martin Staffa <mjstaffa@googlemail.com>
2012-09-11 19:58:31 +0200

    Set the marriage type (CIVIL/RELIGIOUS) in the event type field like MARR_<type>

--- lib/Gedcom/Parser/Fam/Marr.php
+++ lib/Gedcom/Parser/Fam/Marr.php
@@ -21,5 +21,105 @@ namespace Gedcom\Parser\Fam;
  */
 class Marr extends \Gedcom\Parser\Fam\Even
 {
+	 /**
+     *
+     *
+     */
+    public static function parse(\Gedcom\Parser $parser)
+    {
+        $record = $parser->getCurrentLineRecord();
+        $depth = (int)$record[0];
+        
+        $even = new \Gedcom\Record\Fam\Marr();
+        
+        if(isset($record[1]) && strtoupper(trim($record[1])) != 'EVEN')
+            $even->setType(trim($record[1]));
+
+        
+        $parser->forward();
+        
+        while(!$parser->eof())
+        {
+            $record = $parser->getCurrentLineRecord();
+            $recordType = strtoupper(trim($record[1]));
+            $currentDepth = (int)$record[0];
+
+            if($currentDepth <= $depth)
+            {
+                $parser->back();
+                break;
+            }
+            
+            switch($recordType)
+            {
+                case 'TYPE':
+                    $even->setType($even->getType() . '_' .trim($record[2]));
+                break;
+                
+                case 'DATE':
+                    $even->setDate(trim($record[2]));
+                break;
+                
+                case 'PLAC':
+                    $plac = \Gedcom\Parser\Indi\Even\Plac::parse($parser);
+                    $even->setPlac($plac);
+                break;
+                
+                case 'ADDR':
+                    $addr = \Gedcom\Parser\Addr::parse($parser);
+                    $even->setAddr($addr);
+                break;
+                
+                case 'PHON':
+                    $phone = \Gedcom\Parser\Phone::parse($parser);
+                    $even->addPhone($phone);
+                break;
+                
+                case 'CAUS':
+                    $even->setCaus(trim($record[2]));
+                break;
+                
+                case 'AGE':
+                    $even->setAge(trim($record[2]));
+                break;
+                
+                case 'AGNC':
+                    $even->setAgnc(trim($record[2]));
+                break;
+                
+                case 'HUSB':
+                    $husb = \Gedcom\Parser\Fam\Even\Husb::parse($parser);
+                    $even->setHusb($husb);
+                break;
+                
+                case 'WIFE':
+                    $wife = \Gedcom\Parser\Fam\Even\Wife::parse($parser);
+                    $even->setWife($wife);
+                break;
+                
+                case 'SOUR':
+                    $sour = \Gedcom\Parser\SourRef::parse($parser);
+                    $even->addSour($sour);
+                break;
+                
+                case 'OBJE':
+                    $obje = \Gedcom\Parser\ObjeRef::parse($parser);
+                    $even->addObje($obje);
+                break;
+                
+                case 'NOTE':
+                    $note = \Gedcom\Parser\NoteRef::parse($parser);
+                    $even->addNote($note);
+                break;
+                
+                default:
+                    $parser->logUnhandledRecord(get_class() . ' @ ' . __LINE__);
+            }
+            
+            $parser->forward();
+        }
+        
+        return $even;
+    } 
     
 }
